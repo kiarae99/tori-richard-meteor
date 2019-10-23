c74d5ff6f79ac5b8dce169d8aed72b2d262254c1
Kiara Eltagonde <kiarae@hawaii.edu>
2019-10-22 14:21:55 -1000

    Finished Assignment

---
+++ app/imports/ui/pages/ToriRichard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import ToriRichardMain from '../components/ToriRichardMain';
+
+export default class ToriRichard extends React.Component {
+  render() {
+    return (
+        <Container>
+          <ToriRichardMain />
+        </Container>
+    );
+  }
+}
